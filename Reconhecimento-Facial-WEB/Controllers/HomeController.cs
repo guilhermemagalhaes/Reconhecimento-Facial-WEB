20132f23cc0ed11261b9b27abe90bf254f655036
Guilherme Magalhães <guilherme19988@hotmail.com>
2020-06-04 01:10:20 -0300

    Finalizado

---
+++ Reconhecimento-Facial-WEB/Controllers/HomeController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Reconhecimento_Facial_WEB.Models;
+
+namespace Reconhecimento_Facial_WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        const string subscriptionKey = "9be49c63cf354796b1f146154b928f6e";
+
+        const string uriBase = "https://brazilsouth.api.cognitive.microsoft.com/face/v1.0/detect";
+
+        private DadosFace DadosFace = null;
+
+
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new Face());
+        }
+
+        [HttpPost]
+        public IActionResult Index(Face face)
+        {
+
+            ViewBag.DadosFace = null;
+            var urlInterna = UploadedFile(face.Imagem);
+            if (urlInterna != null)
+            {
+                MakeAnalysisRequest(new Uri(urlInterna).AbsolutePath);
+            }
+
+            var imagemname = new Uri(urlInterna).AbsolutePath;
+            imagemname = imagemname.Substring(imagemname.LastIndexOf("/images"));
+
+            while (this.DadosFace == null)
+            {
+                System.Threading.Thread.Sleep(2000);
+            }
+
+            var face2 = new Face()
+            {
+                url_imagem = imagemname,
+                DadosFace = this.DadosFace,
+                name = Environment.UserName
+            };
+
+            face2.DadosFace.FaceAttributes.Gender = face2.DadosFace.FaceAttributes.Gender == "male" ? "Masculino" : "Feminino";
+            face2.DadosFace.FaceAttributes.Glasses = TratarGlasses(face2.DadosFace.FaceAttributes.Glasses);
+            face2.DadosFace.FaceAttributes.Emotion.Anger = TrataEmocao(face2.DadosFace.FaceAttributes.Emotion.Anger);
+            face2.DadosFace.FaceAttributes.Emotion.Neutral = TrataEmocao(face2.DadosFace.FaceAttributes.Emotion.Neutral);
+            face2.DadosFace.FaceAttributes.Emotion.Contempt = TrataEmocao(face2.DadosFace.FaceAttributes.Emotion.Contempt);
+            face2.DadosFace.FaceAttributes.Emotion.Disgust = TrataEmocao(face2.DadosFace.FaceAttributes.Emotion.Disgust);
+            face2.DadosFace.FaceAttributes.Emotion.Happiness = TrataEmocao(face2.DadosFace.FaceAttributes.Emotion.Happiness);
+            face2.DadosFace.FaceAttributes.Emotion.Sadness = TrataEmocao(face2.DadosFace.FaceAttributes.Emotion.Sadness);
+            face2.DadosFace.FaceAttributes.Emotion.Surprise = TrataEmocao(face2.DadosFace.FaceAttributes.Emotion.Surprise);
+
+            return View(face2);
+        }
+
+        private double TrataEmocao(double valor)
+        {
+            return valor * 100;
+        }
+
+        private string TratarGlasses(string glasses)
+        {
+            string ret = "";
+            switch (glasses)
+            {
+                case "NoGlasses":
+                    ret= "Sem óculos";
+                    break;
+                case "Reading Glasses":
+                    ret = "Óculos de leitura";
+                    break;
+                case "Sunglasses":
+                    ret = "Óculos de Sol";
+                    break;
+                case "Swimming Goggles":
+                    ret = "Óculos de Natação";
+                    break;
+            }
+            return ret;
+        }
+
+        private string UploadedFile(IFormFile file)
+        {
+            string filePath = null;
+
+            if (file != null)
+            {
+                string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+                string uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+                filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    file.CopyTo(fileStream);
+                }
+            }
+            return filePath;
+        }
+
+        public async void MakeAnalysisRequest(string imageFilePath)
+        {
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add(
+                    "Ocp-Apim-Subscription-Key", subscriptionKey);
+
+                string requestParameters = "returnFaceId=true&returnFaceLandmarks=false" +
+                "&returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses," +
+                "emotion,hair,makeup,occlusion,accessories,blur,exposure,noise";
+
+                string uri = uriBase + "?" + requestParameters;
+
+                HttpResponseMessage response;
+
+                // Request body. Posts a locally stored JPEG image.
+                byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+                using (ByteArrayContent content = new ByteArrayContent(byteData))
+                {
+                    // This example uses content type "application/octet-stream".
+                    // The other content types you can use are "application/json"
+                    // and "multipart/form-data".
+                    content.Headers.ContentType =
+                        new MediaTypeHeaderValue("application/octet-stream");
+
+                    // Execute the REST API call.
+                    response = await client.PostAsync(uri, content);
+
+                    // Get the JSON response.
+                    string contentString = await response.Content.ReadAsStringAsync();
+
+                    this.DadosFace = JsonConvert.DeserializeObject<List<DadosFace>>(contentString)[0];
+                }
+            }
+
+            static byte[] GetImageAsByteArray(string imageFilePath)
+            {
+                using (FileStream fileStream =
+                    new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
+                {
+                    BinaryReader binaryReader = new BinaryReader(fileStream);
+                    return binaryReader.ReadBytes((int)fileStream.Length);
+                }
+            }
+        }
+    }
+}
---
+++ Reconhecimento-Facial-WEB/Models/Face.cs
@@ -0,0 +1,211 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reconhecimento_Facial_WEB.Models
+{
+    public class Face
+    {
+        public IFormFile Imagem { get; set; }
+        public string url_imagem { get; set; }
+        public DadosFace DadosFace { get; set; }
+
+        public string name { get; set; }
+    }
+
+    public partial class DadosFace
+    {
+        [JsonProperty("faceId")]
+        public Guid FaceId { get; set; }
+
+        [JsonProperty("faceRectangle")]
+        public FaceRectangle FaceRectangle { get; set; }
+
+        [JsonProperty("faceAttributes")]
+        public FaceAttributes FaceAttributes { get; set; }
+    }
+
+    public partial class FaceAttributes
+    {
+        [JsonProperty("smile")]
+        public long Smile { get; set; }
+
+        [JsonProperty("headPose")]
+        public HeadPose HeadPose { get; set; }
+
+        [JsonProperty("gender")]
+        public string Gender { get; set; }
+
+        [JsonProperty("age")]
+        public long Age { get; set; }
+
+        [JsonProperty("facialHair")]
+        public FacialHair FacialHair { get; set; }
+
+        [JsonProperty("glasses")]
+        public string Glasses { get; set; }
+
+        [JsonProperty("emotion")]
+        public Emotion Emotion { get; set; }
+
+        [JsonProperty("blur")]
+        public Blur Blur { get; set; }
+
+        [JsonProperty("exposure")]
+        public Exposure Exposure { get; set; }
+
+        [JsonProperty("noise")]
+        public Noise Noise { get; set; }
+
+        [JsonProperty("makeup")]
+        public Makeup Makeup { get; set; }
+
+        [JsonProperty("accessories")]
+        public object[] Accessories { get; set; }
+
+        [JsonProperty("occlusion")]
+        public Occlusion Occlusion { get; set; }
+
+        [JsonProperty("hair")]
+        public Hair Hair { get; set; }
+    }
+
+    public partial class Blur
+    {
+        [JsonProperty("blurLevel")]
+        public string BlurLevel { get; set; }
+
+        [JsonProperty("value")]
+        public long Value { get; set; }
+    }
+
+    public partial class Emotion
+    {
+        [JsonProperty("anger")]
+        public double Anger { get; set; }
+
+        [JsonProperty("contempt")]
+        public double Contempt { get; set; }
+
+        [JsonProperty("disgust")]
+        public double Disgust { get; set; }
+
+        [JsonProperty("fear")]
+        public double Fear { get; set; }
+
+        [JsonProperty("happiness")]
+        public double Happiness { get; set; }
+
+        [JsonProperty("neutral")]
+        public double Neutral { get; set; }
+
+        [JsonProperty("sadness")]
+        public double Sadness { get; set; }
+
+        [JsonProperty("surprise")]
+        public double Surprise { get; set; }
+    }
+
+    public partial class Exposure
+    {
+        [JsonProperty("exposureLevel")]
+        public string ExposureLevel { get; set; }
+
+        [JsonProperty("value")]
+        public double Value { get; set; }
+    }
+
+    public partial class FacialHair
+    {
+        [JsonProperty("moustache")]
+        public long Moustache { get; set; }
+
+        [JsonProperty("beard")]
+        public long Beard { get; set; }
+
+        [JsonProperty("sideburns")]
+        public long Sideburns { get; set; }
+    }
+
+    public partial class Hair
+    {
+        [JsonProperty("bald")]
+        public double Bald { get; set; }
+
+        [JsonProperty("invisible")]
+        public bool Invisible { get; set; }
+
+        [JsonProperty("hairColor")]
+        public HairColor[] HairColor { get; set; }
+    }
+
+    public partial class HairColor
+    {
+        [JsonProperty("color")]
+        public string Color { get; set; }
+
+        [JsonProperty("confidence")]
+        public double Confidence { get; set; }
+    }
+
+    public partial class HeadPose
+    {
+        [JsonProperty("pitch")]
+        public double Pitch { get; set; }
+
+        [JsonProperty("roll")]
+        public double Roll { get; set; }
+
+        [JsonProperty("yaw")]
+        public double Yaw { get; set; }
+    }
+
+    public partial class Makeup
+    {
+        [JsonProperty("eyeMakeup")]
+        public bool EyeMakeup { get; set; }
+
+        [JsonProperty("lipMakeup")]
+        public bool LipMakeup { get; set; }
+    }
+
+    public partial class Noise
+    {
+        [JsonProperty("noiseLevel")]
+        public string NoiseLevel { get; set; }
+
+        [JsonProperty("value")]
+        public double Value { get; set; }
+    }
+
+    public partial class Occlusion
+    {
+        [JsonProperty("foreheadOccluded")]
+        public bool ForeheadOccluded { get; set; }
+
+        [JsonProperty("eyeOccluded")]
+        public bool EyeOccluded { get; set; }
+
+        [JsonProperty("mouthOccluded")]
+        public bool MouthOccluded { get; set; }
+    }
+
+    public partial class FaceRectangle
+    {
+        [JsonProperty("top")]
+        public long Top { get; set; }
+
+        [JsonProperty("left")]
+        public long Left { get; set; }
+
+        [JsonProperty("width")]
+        public long Width { get; set; }
+
+        [JsonProperty("height")]
+        public long Height { get; set; }
+    }
+}
